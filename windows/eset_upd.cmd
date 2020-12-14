8a0ca50b572997ede6163b0aab3fdc0d6aed4c8f
aDio <figo_shen@qq.com>
2020-12-14 14:06:04 +0800

    Create eset_upd.cmd

--- windows/eset_upd.cmd
+++ windows/eset_upd.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 REM    -------- Update version  --------- 
-set mVer=ep7dll v13dll
+set mVer=ep7dll
 REM    -------- ID & PW  ---------  
 set ID=EAV-0267750251
 set PW=3vr2krbn3s
@@ -14,16 +14,16 @@ set here=%~dp0
 set wget=%~dps0wget.exe
 set tmpfile=%tmp%\update.log
 set source=%ID%:%PW%@%url%
-
+set oPath=c:\www\eset_up
 call :version %mVer% 
 timeout 10
 exit
-
+REM ------------------------------
 :version
 set ver=%1
 if exist %tmpfile% del /Q %tmpfile%
 if not exist %oPath%\%ver:dll=\dll%\nul md %oPath%\%ver:dll=\dll%
-wget% -N  -O%tmp%/update.ver http://%source%/eset_upd/%ver:dll=/dll%/update.ver
+%wget% -N  -O%tmp%/update.ver http://%source%/eset_upd/%ver:dll=/dll%/update.ver
 if exist %tmp%\%ver%.ver  @fc /A /L /LB1 %tmp%\update.ver %tmp%\%ver%.ver && goto next
 sed -n "/PICO/,/inte/d;/\[[^H\|^CO]/,/size/p"  %tmp%/update.ver> %tmp%\%ver%.ver
 sed -n "/_l[0-9]/s@file=@http://%url%@ p" %tmp%\%ver%.ver >%tmp%\%ver%.lst
@@ -32,7 +32,7 @@ if exist %tmp%\%ver%.log del /f %tmp%\%ver%.log
 sed "s@file=\/.*\/@file=@g"  %tmp%\%ver%.ver > eset_upd\%ver:dll=\dll%\update.ver
 goto :next
 exit
-
+REM ------------------------------
 :next
 cd %here%
 move /y %tmp%\update.ver %tmp%\%ver%.ver
